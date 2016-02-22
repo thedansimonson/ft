e8c93bb428de020bad413eaa626855a9a810dade
Dan Simonson <dolus.occuli@gmail.com>
2016-02-22 04:28:38 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import ft
+
+setup(name="ft",
+        version=str(ft.version),
+        description="Data management and manipulation package.",
+        author="Dan Simonson",
+        author_email="dan.simonson@gmail.com",
+        url="http://www.thedansimonson.com/ft/",
+        packages=["ft"],
+        )
---
+++ speed_test.py
@@ -0,0 +1,23 @@
+import ft, datetime
+from pprint import pprint
+
+data = ft.load_csv(open("mid_e_verb_morph.csv"))
+
+print "ft.summary(data): "
+pprint(ft.summary(data))
+
+
+print "Speed tests..."
+data = data * 50
+tests = {ft.indexBy0: [], ft.indexBy2: [], ft.indexBy3:[], ft.indexBy4: []}
+for i in range(1,10000):
+    for f in tests: 
+        start = datetime.datetime.now()
+        datadex = f("mud", data)
+        stop = datetime.datetime.now()
+        tests[f].append((stop-start).total_seconds())
+
+for f in tests:
+    print f, float(sum(tests[f]))/len(tests[f])
+
+    
